2074e15d5f5477737f5e39f0f5a02d91db9def47
Tsz Tin Vivian Soo <tsztin0217@gmail.com>
2025-09-12 16:36:52 -0400

    Create helper function to provide available list of letters to use and modify draw_letters function so that a random letter will be picked into the hand

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,7 +1,55 @@
 from random import randint
 
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
+# create helper function to create a list of all available letters to use
+def create_letter_list():
+    letters = ""
+
+    for letter, quantity in LETTER_POOL.items():
+        if quantity > 0:
+            letters += (letter * quantity)
+
+    letter_list = list(letters)
+    return letter_list
+
 def draw_letters():
-    pass
+    letter_list = create_letter_list()
+    hand = []
+    while len(hand) < 10:
+        random_index = randint(0, len(letter_list))
+        hand.append(letter_list[random_index])
+        letter_list.pop(random_index)
+    return hand
+
+
 
 def uses_available_letters(word, letter_bank):
     pass
